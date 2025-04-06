61aca1314879c47ebf62fe35cc2b658bd32b3897
Fereshteh Rohani <roha0011@algonquinlive.com>
2025-04-06 00:02:22 -0400

    Create useGame to fetch the gmes

--- src/components/ui/GameGrid.tsx
+++ src/components/ui/GameGrid.tsx
@@ -2,36 +2,17 @@
 /* @fileoverview GameGrid
 /* @author Fereshteh Rohani
 /* @created 2024-04-05
-/* @modified 2024-04-05
+/* @modified 2024-04-06
 /********** ********** ********** **********/
 
-import apiClient from "@/services/api-client";
-import { useEffect, useState } from "react";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface GamesResponse {
-  count: number;
-  results: Game[];
-}
+import { Text } from "@chakra-ui/react";
+import useGames from "@/hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<GamesResponse>("./games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
-
+  const { games, error } = useGames();
   return (
     <>
-      {error && <text>{error}</text>}
+      {error && <Text>{error}</Text>}
       <ul>
         {games.map((game) => (
           <li key={game.id}>{game.name}</li>
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,42 @@
+/********** ********** ********** **********/
+/* @fileoverview GameGrid
+/* @author Fereshteh Rohani
+/* @created 2024-04-05
+/* @modified 2024-04-06
+/********** ********** ********** **********/
+
+import apiClient from "@/services/api-client";
+import { useEffect, useState } from "react";
+import { CanceledError } from "axios";
+
+interface Game {
+  id: number;
+  name: string;
+}
+
+interface GamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+    const [games, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState("");
+  
+    useEffect(() => {
+        const controller = new AbortController();
+
+      apiClient
+        .get<GamesResponse>("./games", { signal: controller.signal })
+        .then((res) => setGames(res.data.results))
+        .catch((err) => {
+            if (err instanceof CanceledError) return;
+            setError(err.message)});
+
+        return () => controller.abort();
+    }, []);
+
+    return { games, error };
+}
+
+export default useGames;
